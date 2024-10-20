d166af44517eeb750c3e113ba88da9c0e3c45443
Sulaiman Mulla <sulaiman_1@tamu.edu>
2024-10-20 06:32:06 -0500

    updated gemini input for more detail

--- gemini.py
+++ gemini.py
@@ -8,5 +8,5 @@ genai.configure(api_key=os.environ["API_KEY"])
 #generate response from file
 def generate_info(json_input, prompt):
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(f"{prompt}\n\n{json_input}")
+    response = model.generate_content(f"My question is: {prompt}\n\n and the following is my record: {json_input} \n\n summarize my record before answering my question")
     return response.text
