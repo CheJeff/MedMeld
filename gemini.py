6d2e40b10ce07a4e4a2ff027bb7e390e19af15f0
Sulaiman Mulla <sulaiman_1@tamu.edu>
2024-10-19 20:10:18 -0500

    added gemini connection

---
+++ gemini.py
@@ -0,0 +1,16 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.environ["API_KEY"])
+
+#generate response from file
+def generate_info(json_input, prompt):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(f"{prompt}\n\n{json_input}")
+    return response.text
+
+#prompt = "" #ask user
+#response_text = generate_info(content, prompt)
+#print(response_text)
